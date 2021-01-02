6d08e65bb2b3307de61678ae1555cfdd55164c00
Jingchao Yu <jingchaoyu@hotmail.com>
2021-01-02 22:07:37 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,19 +7,15 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  [{:name=>"Don Gately", :occupation=>"Live-in Staff"},
-   {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
-    {:name=>"Pat Monteseian", :occupation=>"Staff"},
-    {:name=>"Kate Gompert", :occupation=>"None"},
-    {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}]
+  [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
 end
 
 def literal_aoh
-  [ {DON_G: { name:  "Don Gately", occupation: "Live-in Staff" }},
-    {JOELLE_VD: { name:  "Joelle van Dyne", occupation: "Radio Personality" }},
-    {PAT_M: { name:  "Pat Monteseian", occupation: "Staff" }}, 
-    {KATE_G: { name:  "Kate Gompert", occupation: "None" }},
-    {BRUCE_G: { name:  "Bruce Green", occupation: "Fan of Mildred" }}
+  [ {name:  "Don Gately", occupation: "Live-in Staff" },
+    {name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    {name:  "Pat Monteseian", occupation: "Staff" }, 
+    {name:  "Kate Gompert", occupation: "None" },
+    {name:  "Bruce Green", occupation: "Fan of Mildred" }
   ]
 end
 
@@ -29,4 +25,5 @@ end
 
 def aoh_update(aoh, row, key, new_value)
   aoh[row][key] = new_value
+  return aoh
 end
